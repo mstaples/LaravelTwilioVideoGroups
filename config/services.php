6607c6ba380e3d0986eb81d5fe64f73d73039134
mstaples <margaret.staples@gmail.com>
2017-09-15 15:50:25 +0000

    initial commit

---
+++ config/services.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    // sid and token are needed to access the standard Twilio Rest API
+    // key and secret are additionally needed to access the Twilio Video API
+    'twilio' => [
+        'sid' => env('TWILIO_ACCOUNT_SID'),
+        'token' => env('TWILIO_ACCOUNT_TOKEN'),
+        'key' => env('TWILIO_API_KEY'),
+        'secret' => env('TWILIO_API_SECRET')
+    ]
+];
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Laravel and Twilio Video API Demo Project
+
